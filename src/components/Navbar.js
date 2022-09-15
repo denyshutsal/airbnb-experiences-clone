41f3e8405ac61987022731482035acef0287f860
Denys Hutsal <denyshutsal@gmail.com>
2022-09-15 14:02:21 +0200

    feat: add Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="/images/airbnb-logo.svg" width="83" alt="airbnb-logo"/>
+    </nav>
+  )
+}
+
