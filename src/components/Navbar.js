6f8c6dff03bc178bc3d770806b2b0846f41e4554
Denys Hutsal <denyshutsal@gmail.com>
2022-09-15 14:59:37 +0200

    refactor: change structure of the navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,10 @@ import React from "react"
 
 export default function Navbar() {
   return (
-    <nav>
-      <img src="/images/airbnb-logo.svg" width="83" alt="airbnb-logo"/>
+    <nav className="main-nav">
+      <div className="main-nav__container container">
+        <img src="/images/airbnb-logo.svg" width="83" alt="airbnb-logo"/>
+      </div>
     </nav>
   )
 }
