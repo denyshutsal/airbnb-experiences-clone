21711ccdfb792c08d281d863fabe9d94bdf0dd33
Denys Hutsal <denyshutsal@gmail.com>
2022-09-15 16:12:54 +0200

    feat: add Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero__container container">
+        <h1 className="hero__title">Online Experiences</h1>
+        <p className="hero__text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+</p>
+      </div>
+    </section>
+  )
+}
