e7433e7ddabb2b4c1aabc51ea02db055f518ed0c
Denys Hutsal <denyshutsal@gmail.com>
2022-09-17 11:18:05 +0200

    refactor: change structure of the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
 
 export default function App() {
+
+  const cards = data.map(card => {
+    return (
+      <Card
+        key={card.id.toString()}
+        {...card}
+      />
+    )
+  })
+
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <section className="cards">
+        <div className="cards__container container">
+          {cards}
+        </div>
+      </section>
     </div>
   )
 }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,26 +1,11 @@
 import React from "react"
-import Card from "./Card"
-import data from "./../data"
 
 export default function Hero() {
-
-  const cards = data.map(card => {
-    return (
-      <Card
-        key={card.id.toString()}
-        {...card}
-      />
-    )
-  })
-
   return (
     <section className="hero">
       <div className="hero__container container">
         <h1 className="hero__title">Online Experiences</h1>
         <p className="hero__text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
-        <div className="hero__cards">
-          {cards}
-        </div>
       </div>
     </section>
   )
